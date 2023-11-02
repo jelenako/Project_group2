749aa3e66f2080a2393f78ca4fb36f6ae37db658
dbunic <dejan.bunic@ttcontrol.com>
2023-11-02 13:19:40 +0100

    Update 1

--- functions.py
+++ functions.py
@@ -4,24 +4,24 @@ class User:
     def __init__(self, username, password):
 
         if len(password) > 12:
-            raise ValueError("Prekoracena duzina") 
+            raise ValueError("Length greater than 12") 
         
-        temp = int(password)
+        int(password)
 
-        self._username = username
-        self._password = password
-        self._list_of_contacts = []
+        self.__username = username
+        self.__password = password
+        self.__list_of_contacts = []
 
     def __len__(self):
-        return len(self._list_of_contacts)
+        return len(self.__list_of_contacts)
     
     def __str__(self):
-        return f"Username: {self._username} Password: {self._password}"
+        return f"Username: {self.__username} Password: {self.__password}" #kako ispis 
     
     def delete(self, username):
-        for contact in self._list_of_contacts :
-            if(contact.usernam == username):
-                self._list_of_contacts.remove(contact)
+        for contact in self.__list_of_contacts :
+            if(contact == username):
+                self.__list_of_contacts.remove(contact)
 
 
     def __iter__(self):
@@ -29,8 +29,8 @@ class User:
         return self
     
     def __next__(self):
-        if self.n < len(self._list_of_contacts):
-            result = self._list_of_contacts[self.n]
+        if self.n < len(self.__list_of_contacts):
+            result = self.__list_of_contacts[self.n]
             self.n += 1
             return result
         else:
@@ -39,31 +39,30 @@ class User:
 
     @property
     def username(self):
-        return self._username
+        return self.__username
     
     @username.setter
     def username(self, username):
-        self._username = username
+        self.__username = username
         
     @property
     def password(self):
-        return self._password
+        return self.__password
     
     @password.setter
     def password(self, password):
-        self._password = password
+        self.__password = password
 
     @property
-    def list_of_contracts(self):
-        return self._list_of_contacts
+    def list_of_contacts(self):
+        return self.__list_of_contacts
     
-    @list_of_contracts.setter
-    def list_of_contracts(self, user):
-        self._list_of_contacts.append(user)
+    @list_of_contacts.setter
+    def list_of_contacts(self, user):
+        self.__list_of_contacts.append(user)
     
     
 user_list = []
-
 logged_users = []
 
 
@@ -71,20 +70,29 @@ def register_user(username, password):
     user = User(username, password)
     if get_user(username) is None:
         user_list.append(user)
+    else:
+        print(f"Username: {username} already exists.")
     
 
 def login(username, password):
-   user = get_user(username)
-   if user is None:
-       print("You need to register this user")
-       return
-   
-   if (user.password == password):
-    #    print("Treba pokrenuti novi proces koji ne radi nista")
-        process = multiprocessing.Process(target=print_login_message(user))
-        process.start()
-        process.join()
-        logged_users.append(user)
+    if not is_user_logged_in(username):
+        user = get_user(username)
+        if user is None:
+            print("You need to register this user. Try to log in again.")
+            return
+    
+        if (user.password == password):
+            process = multiprocessing.Process(target=print_login_message(user))
+            process.start()
+            process.join()
+            logged_users.append(user.username)
+        else:
+            print("Not valid password. Please log in again.")
+    else:
+        print(f"You {username} are already logged in")
+
+    
+
 
 def get_user(username):
     for user in user_list:
@@ -92,63 +100,80 @@ def get_user(username):
             return user
     return None
 
+
+def is_user_logged_in(username):
+    for user in logged_users:
+        if user == username:
+            return True
+    # print(f"You {username} not logged in.")
+    return False
+
+
 def print_login_message(user):
     time.sleep(1)
     print(f"User {user.username} is logged in.")
 
 
 def logout(username):
-    for user in logged_users:
-        if user.username == username:
-            logged_users.remove(user)
-            print("User logout")
-            return
-    print("You are not logged")
+    if not is_user_logged_in(username):
+        print(f"You {username} are not logged in")
+        return 
+    
+    logged_users.remove(username)
+    print(f"User with {username} log out")
 
 
+def add_new_contact(username, contact):
+    if is_user_logged_in(username):
+        if get_user(contact) is not None:
+            get_user(username).list_of_contacts = contact
+        else:
+            print(f"Contact {contact} not registered")
+    else:
+        print(f"You {username} not logged in.")
 
 
-def add_new_contract(user, contract):
-    for logged_user in logged_users:
-        if user.username == logged_user.username :
-            user.list_of_contracts = contract
+def delete_contact(username, contact):
+    if is_user_logged_in(username):
+        get_user(username).delete(contact)
+    else:
+        print(f"You {username} not logged in.")
 
-def delete_contract(user, contract):
-    for logged_user in logged_users:
-        if user.username == logged_user.username :
-            user.delete(contract.username)
-
-def print_contract(user):
-    for logged_user in logged_users:
-        if user.username == logged_user.username :
-            iterator = iter(user)
-            try:
-                while True:
-                 item = next(iterator)
-                 print(item)
-            except StopIteration:
-                pass
+def print_contact(username):
+    if is_user_logged_in(username):
+        user = get_user(username)
+        iterator = iter(user)
+        try:
+            while True:
+                print(next(iterator))
+        except StopIteration:
+            pass
+    else:
+        print(f"You {username} not logged in.")
             
                
 
 
 if __name__ == "__main__":
-    print("Dobrodosao")
-    user1 = User("dejan", "1111111")
-    user2 = User("neko", "1111")
-    user3 = User("pero", "222")
-    # user1.password = "444"
-    user1.list_of_contracts = user2
-    user1.list_of_contracts = user3
+   try:
+        user1 = User("dejan", "11111")
+        user2 = User("pero", "11111")
+        user3 = User("slavko", "11111")
 
-    register_user("dejan", "1111111")
-    login("dejan", "1111111")
-    # time.sleep(2)
-    logout("dejan")
-    # print(len(user1))
+        register_user(user1.username, user1.password)
+        register_user(user2.username, user2.password)
+        register_user(user3.username, user3.password)
 
-    # iterator = iter(user1)
+        for user in user_list:
+            print(user)
 
-    # print(next(iterator))
-    # print(next(iterator))
-    
+        login(user1.username, user1.password)
+        add_new_contact(user1.username, user2.username)
+        add_new_contact(user1.username, user3.username)
+
+        # get_user(user1.username).delete("slavko")
+        print("eeeeeeeeeeeee")
+        print_contact(get_user(user1))
+
+   except Exception as e:
+       print(e)
