de6a7f3396e26e53b8185aad8ffe18cf732248e1
tpredojevic <tanjapredojevic78@gmail.com>
2023-11-06 18:18:19 +0100

    lib

--- hash_module.py
+++
@@ -1,9 +0,0 @@
-import pythonhashmodule
-
-hashed_password = pythonhashmodule.hash_password("my_password")
-print(hashed_password)
-
-result = pythonhashmodule.check_password(hashed_password, "my_password")
-print(result)
-
-
--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module = Extension('pythonhashmodule', sources=['hash.c'])
-
-setup(name='pythonhashmodule',
-      version='1.0',
-      description='Python module for password hashing',
-      ext_modules=[module])
-
